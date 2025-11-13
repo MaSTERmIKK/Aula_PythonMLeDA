9caf9f248caac87e8c0d25f8e1911cc569c029c0
Mirko Campari <82829377+MaSTERmIKK@users.noreply.github.com>
2025-11-13 11:34:23 +0100

    Add files via upload

---
+++ Giornate_corso/Giovedi 13_11/es_cicli.py
@@ -0,0 +1,36 @@
+
+controllo = True
+
+while controllo:
+    print(controllo)
+    controllo = False
+
+
+controllor = True
+# ciclo booleano
+while controllor:
+    scelta = input("vuoi continuare?")
+    if scelta.lower() == "no":
+        controllor= False
+    else:
+        print("stai continuando")
+   
+        
+x = 0
+# ciclo matematico
+while x < 5:
+    x+=1
+    print(x)
+    
+    
+lisTa = [1,2,3,4,5]
+for pippo in lisTa:
+    print(pippo)
+    
+    
+parola = "mirko"
+for pippo in parola:
+    x = 0
+    print (parola[x])
+    x +=1
+    
